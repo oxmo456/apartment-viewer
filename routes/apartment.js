baf1ef89d95ca448767437d7b5a822b2ffa82823
Nicolas Markovic <nicolas.anto.markovic@gmail.com>
2014-07-06 18:33:44 -0400

    ...

--- routes/apartment.js
+++ routes/apartment.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var request = require('request');
 
+console.log(process.env);
+
+var FANA_SERVER = process.env.FANA_SERVER || "http://localhost:8080/";
 
 module.exports = function (req, res) {
 
     var apartmentId = req.params.id;
 
-    request('http://localhost:8080/apartment/' + apartmentId, function (error, response, body) {
+    request(FANA_SERVER + 'apartment/' + apartmentId, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             res.render('apartment', JSON.parse(body));
         } else {
